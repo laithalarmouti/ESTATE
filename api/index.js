bca0fa3c597cca044e20880f3ae191ea9e5e1375
Laith <Laithalarmouti@gmail.com>
2024-02-27 20:03:59 +0300

    creating a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -19,11 +19,22 @@ const app=express();
 
 app.use(express.json());
 
-
-
-
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server ERROR';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
 
 
 
