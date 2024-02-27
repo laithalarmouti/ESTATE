251ac536dc70d6298b0e9081565fb4c38a87b638
Laith alarmouti <laithalarmouti@gmail.com>
2024-02-27 13:19:25 +0300

    Create SignUp API route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
+
+
+
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connection to Database is Successful')
@@ -12,10 +17,13 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app=express();
 
+app.use(express.json());
+
 
 
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
 
 
 
