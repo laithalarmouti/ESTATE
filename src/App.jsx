04984e5eb9d45c95bb01c2c57ab2ef952f62158d
Laith alarmouti <laithalarmouti@gmail.com>
2024-02-27 09:49:51 +0300

    Created The Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import About from './pages/About'
+import Header from './components/Header';
 
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
