e98ea81ba5149303818fcd90f64d95dff1509631
Laith alarmouti <laithalarmouti@gmail.com>
2024-02-28 12:03:00 +0300

    ErrorHandling bug fixes

--- api/utils/error.js
+++ api/utils/error.js
@@ -2,7 +2,7 @@
 
 
 export const errorHandler = (statusCode, message)=>{
-    const error = new error()
+    const error = new Error()
     error.statusCode = statusCode
     error.message = message
     return error
