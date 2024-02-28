e119873a6a6575897ece0e498abdc109bf118924
Laith alarmouti <laithalarmouti@gmail.com>
2024-02-28 12:56:57 +0300

    Add REDUX TOOLKIT

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInFailure, signInSuccesss } from '../redux/user/userSlice'
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false);
+  const { loading, error} = useSelector((state)=>state.user);
 
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
     setFormData({
       ...formData,
@@ -19,7 +20,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
+      dispatch(signInStart());
       const res = await fetch('api/auth/signin', 
         {
           method: 'POST',
@@ -31,16 +32,13 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if(data.success ===false){
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
         return;
       }
-      setLoading(false)
-      setError(null)
+      dispatch(signInSuccesss(data))
       navigate('/')
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message));
     }
   }
 
---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
+import userReducer from './user/userSlice'
+
+export const store = configureStore({
+  reducer: {user: userReducer},
+  middleware: (buildGetDefaultMiddleware)=> buildGetDefaultMiddleware({
+    serializableCheck: false,
+  }),
+})
