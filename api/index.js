bceef6b398f257d226ae4f2c293969a5e86e77e9
Laith alarmouti <laithalarmouti@gmail.com>
2024-03-05 15:45:11 +0300

    create User Update Api routes and features

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 
 
 
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
