5eb7827a92b246479671c9de7084800271d25d1a
Laith <Laithalarmouti@gmail.com>
2024-03-09 19:06:06 +0300

    added a signout functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { google, signup } from '../controllers/auth.controller.js';
 import { signin } from '../controllers/auth.controller.js';
+import { signOut } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
@@ -8,4 +9,5 @@ const router = express.Router();
 router.post("/signup", signup)
 router.post("/signin", signin)
 router.post('/google', google)
+router.get('/signout', signOut)
 export default router;
